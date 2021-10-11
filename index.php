1f20329e0e1c1c56e60842269634e5e52996a9fc
Antonio <antonio.sanna2706@gmail.com>
2021-10-11 16:43:53 +0200

    Scafolding

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    $faq = [
+        [
+            'Domanda'=> 'Come state implementando la recente decisione della Corte di giustizia dell Unione europea (CGUE) relativa al diritto all oblio?',
+            'Risposta'=> '<p>La recente <a href="#"> decisione della Corte di giustizia dell Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</p> '
+        ]
+    ]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Faq</title>
+</head>
+<body>
+    <section>
+        
+    </section>
+</body>
+</html>
